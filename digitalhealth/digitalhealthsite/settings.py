710e19e2031f028925f7521f88dc679d69b041b3
Kaylee Agorilla <kayleeag@hawaii.edu>
2025-04-17 07:13:33 -1000

    Update settings.py

--- digitalhealth/digitalhealthsite/settings.py
+++ digitalhealth/digitalhealthsite/settings.py
@@ -93,16 +93,31 @@ WSGI_APPLICATION = 'digitalhealthsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         "NAME": "digitalhealthdb",
+#         "USER": "root",
+#         "PASSWORD": "abc12345",
+#         "HOST": "127.0.0.1",
+#         "PORT": "3306",
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        "NAME": "digitalhealthdb",
-        "USER": "root",
-        "PASSWORD": "abc12345",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'ssl': {'ssl-mode': 'REQUIRED'},
+        },
     }
 }
+
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
